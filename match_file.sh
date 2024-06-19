17486c97faa4718f7a284d1c8f8483e53a763d6b
James Bonfield <jkb@sanger.ac.uk>
2024-06-19 09:10:24 +0100

    Add Apache 2.0 license

--- match_file.sh
+++ match_file.sh
@@ -10,7 +10,7 @@ ga=/nfs/sam_scratch/jkb/conda22/bin/GraphAligner
 
 # Produce the training gaf
 $ga -g $gfa -f $train -x vg -a train.gaf >/dev/null
-./gaf2gfa.pl train.gaf $train $gfa > $gfa.nodeseq
+./gaf2nodeseq.pl train.gaf $train $gfa > $gfa.nodeseq
 
 # Compare each query against the nodeseq
 samtools faidx $query
