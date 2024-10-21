33f37c74f5c78ececc0144f01c03aaafef9f01a6
James Bonfield <jkb@sanger.ac.uk>
2024-10-21 10:20:33 +0100

    Add path2seq.pl script.
    Based on gaf2seq, but handles both strands and works on GAF path
    strings only so easier to feed in from other sources.

---
+++ path2seq.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+# Turn a GFA + GAF alignment path string ">node>node<node..." into a
+# sequence by copying the elements out of the GFA and stitching them
+# together.
+
+# Usage: path2seq.pl in.gfa path
+
+# Parse GFA; minimally
+open(my $gfa, "<", shift(@ARGV)) || die;
+while (<$gfa>) {
+    chomp($_);
+    my @F = split(/\s+/, $_);
+    next unless scalar(@F) && $F[0] eq "S"; # skip other fields for now
+    $gfa{$F[1]}{seq} = uc($F[2]);
+}
+
+
+# Parse the path
+while (<>) {
+    chomp();
+    my $seq="";
+
+    foreach ($_=~m/[<>][^<>]*/g) {
+	my ($dir,$node) = ($_=~m/(.)(.*)/);
+	my $gseq = $gfa{$node}{seq};
+	if ($dir eq "<") {
+	    $gseq =~ tr/ACGT/TGCA/;
+	    $gseq = reverse($gseq);
+	}
+	$seq .= $gseq;
+    }
+    
+    print "$seq\n";
+}
