ce4e92bbe0fe2a384b2118b4457ae968bdbb24b1
Josh Cudby <jjcc2@cam.ac.uk>
2025-08-14 17:06:02 +0100

    Fix logic for default qubo solver run

--- tangle_resolution_benchmark.sh
+++ tangle_resolution_benchmark.sh
@@ -4,9 +4,18 @@ time_limits="$2"
 num_jobs="$3"
 num_training="$4"
 solvers="$5"
+pathfinder="$6"
 
 export QDIR=${QDIR:-$(pwd)}
 
-parallel --header  : "$QDIR/run_gfa_sim.sh" -s {seed} -c "$QDIR"/config_illumina_{annotate}.sh \
--a {annotate} --solver {solver} -p {solver}.{annotate}. -t $time_limits -j $num_jobs -n $num_training --pathfinder \
-::: seed $(seq 1 "$max_seed") ::: annotate mg km ga ::: solver ${solvers}
+if [ "$pathfinder" -eq 1 ]; then
+
+    parallel --header  : "$QDIR/run_gfa_sim.sh" -s {seed} -c "$QDIR"/config_illumina_{annotate}.sh \
+    -a {annotate} --solver {solver} -p {solver}.{annotate}. -t $time_limits -j $num_jobs -n $num_training --pathfinder \
+    ::: seed $(seq 1 "$max_seed") ::: annotate mg km ga ::: solver ${solvers}
+
+else
+    parallel --header  : "$QDIR/run_gfa_sim.sh" -s {seed} -c "$QDIR"/config_illumina_{annotate}.sh \
+    -a {annotate} --solver {solver} -p {solver}.{annotate}. -t $time_limits -j $num_jobs -n $num_training \
+    ::: seed $(seq 1 "$max_seed") ::: annotate mg km ga ::: solver ${solvers}
+fi
