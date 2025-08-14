8401d23df9c96e10f0b1bc91aaa88259abbff78d
Josh Cudby <jjcc2@cam.ac.uk>
2025-08-14 12:15:24 +0100

    Faster qubo matrix creation (with some documentation)

--- qubo/qubo_solvers/oriented_tangle/utils/qubo_utils.py
+++ qubo/qubo_solvers/oriented_tangle/utils/qubo_utils.py
@@ -19,7 +19,7 @@ def edge2node_qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None
     """
     nodes = list(graph.nodes)
     V = int(len(nodes) / 2)
-    total_weight = int(sum(graph.nodes[node]["weight"] for node in nodes))
+    total_weight = sum(graph.nodes[node]["weight"] for node in nodes)
     
     # T_max = total weight + "a bit"
     if alpha is None:
@@ -39,55 +39,32 @@ def edge2node_qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None
     logger.info(f'Penalties. t: {lambda_t}, g: {lambda_g}, w: {lambda_w}')
     
     # Note: we add an end node with parity 0 and 1, we only want 1 of them. We will delete the other at the end.
-    qubo_matrix = np.zeros((T_max, V + 1, 2, T_max, V + 1, 2), dtype=np.int16)
+    qubo_matrix = np.zeros((T_max, V + 1, 2, T_max, V + 1, 2), dtype=float)
     
-    # Path constraint
+    # Path constraint: for each t
+    # ( sum_{i,b} ( x_{t,i,b} ) - 1 ) ^2 
+    # = sum_{(i1,b1), (i2,b2)} ( x_{t,i1,b1} x_{t,i2,b2} ) + sum_{i,b} ( - 2 * x_{t,i,b} )  +  1        
+    T_qubo_matrix = lambda_t * (
+        np.ones((V+1, 2, V+1, 2), dtype=float) # + sum_{(i1,b1),(i2,b2)} x_{t,i1,b1} x_{t,i2,b2}
+        - 2 * np.array([1]+ ([0]*2*(V+1)+[1]) * (2*(V+1)-1), dtype=float).reshape((V+1,2,V+1,2)) # -2 * sum_{i,b} ( - x_{t,i,b} )
+    )
     for t in range(T_max):
-        for i, b in product(range(V), range(2)):
-            qubo_matrix[t, i, b, t, i, b] -= lambda_t
-            qubo_matrix[t, V, 0, t, i, b] += 2 * lambda_t
-                
-        qubo_matrix[t, V, 0, t, V, 0] -= lambda_t
-        
-        for i, j, bi, bj in product(range(V), range(V), range(2), range(2)):
-            if not (i == j and bi == bj):
-                qubo_matrix[t, i, bi, t, j, bj] += lambda_t
+        qubo_matrix[t, :, :, t, :, :] += T_qubo_matrix
     
-    # Set start/end nodes
-    start_nodes=set()
-    end_nodes= set()
-    for node, val in dict(graph.nodes.data('start')).items():
-        if val == 'start':
-            print(f'Found start node:{node}')
-            node_index_in_qubo = floor(nodes.index(node)/ 2)
-            start_nodes.add(node_index_in_qubo)
-        if val == 'end':
-            print(f'Found end node:{node}')
-            node_index_in_qubo = floor(nodes.index(node)/ 2)
-            end_nodes.add(node_index_in_qubo)
     
-    start_nodes = list(start_nodes)        
-    end_nodes = list(end_nodes)
-    print(f'Start nodes: {start_nodes}, End nodes: {end_nodes}')
-    exist_start_nodes = len(start_nodes) > 0
-    exist_end_nodes = len(end_nodes) > 0
-    
-    # Graph step constraints
+    # Graph step constraints: for each (t, t+1)
+    # 1 - sum_{i1, b1} ( x_{t,i1,b1} * ( sum_{i2,b2 neighbour i1, b1} x_{t+1,i2,b2} ) )
+    G_qubo_matrix = np.zeros((V+1, 2, V+1, 2), dtype=float)
+    G_qubo_matrix[:, :, V, 0] = 0
+    for u, v in graph.edges:
+        u_idx = nodes.index(u)
+        i, bi = u_idx // 2, u_idx % 2
+        v_idx = nodes.index(v)
+        j, bj = v_idx // 2, v_idx % 2
+        G_qubo_matrix[i, bi, j, bj] = 0
     for t in range(T_max - 1):
-        for i, j, bi, bj in product(range(V), range(V), range(2), range(2)):
-            if (nodes[2 * i + bi], nodes[2 * j + bj]) not in graph.edges:
-                qubo_matrix[t, i, bi, t+1, j, bj] += lambda_g
-        for i, bi in product(range(V), range(2)):
-            qubo_matrix[t, V, 0, t+1, i, bi] += 5 * lambda_g
-            if exist_end_nodes:
-                if i not in end_nodes:
-                    qubo_matrix[t, i, bi, t+1, V, 0] += lambda_g
-    if exist_start_nodes:
-        start_node = start_nodes[0]
-        for b in range(2):
-            qubo_matrix[0, start_node, b, 0, start_node, b] -= lambda_g
-            qubo_matrix[0, start_node, b, 0, start_nodes, 1 - b] += lambda_g
-        
+        qubo_matrix[t, :, :, t+1, :, :] = G_qubo_matrix
+            
                 
     # Weights constraints
     # ( sum_t ( x_{t,i,b} ) - w[i, b] ) ^2 = sum_t ( (1 - 2 * w[i, b]) x_{t,i,b} ) + sum_{t1 /= t2} x_{t1,i,b} x_{t2,i,b} + w[i,b]^2
@@ -107,7 +84,7 @@ def edge2node_qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None
     qubo_matrix = np.delete(qubo_matrix, [np.ravel_multi_index((t, V, 1), dims=(T_max, V+1, 2)) for t in range(T_max)], 0)
     qubo_matrix = np.delete(qubo_matrix, [np.ravel_multi_index((t, V, 1), dims=(T_max, V+1, 2)) for t in range(T_max)], 1)
     
-    offset = lambda_t * T_max  + lambda_w * int(sum(graph.nodes[nodes[2 * i + b]]["weight"] ** 2 for i in range(V) for b in range(2))) + (1 if exist_start_nodes else 0)  * lambda_g
+    offset = lambda_t * T_max  + lambda_w * sum(graph.nodes[nodes[2 * i + b]]["weight"] ** 2 for i in range(V) for b in range(2))
     
     # normalisation = np.max(np.abs(qubo_matrix))
     # qubo_matrix = qubo_matrix / normalisation
@@ -128,7 +105,7 @@ def qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None, penaltie
     """
     nodes = list(graph.nodes)
     V = int(len(nodes) / 2)
-    total_weight = int(sum(graph.nodes[node]["weight"] for node in nodes) / 2)
+    total_weight = sum(graph.nodes[node]["weight"] for node in nodes) / 2
     
     # T_max = total weight + "a bit"
     if alpha is None:
@@ -147,67 +124,51 @@ def qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None, penaltie
         lambda_w = penalties[2]
     logger.info(f'Penalties. t: {lambda_t}, g: {lambda_g}, w: {lambda_w}')
     
-    # Note: we add an end node with parity 0 and 1, we only want 1 of them. We will delete the other at the end.
-    qubo_matrix = np.zeros((T_max, V + 1, 2, T_max, V + 1, 2), dtype=np.int16)
+        # Note: we add an end node with parity 0 and 1, we only want 1 of them. We will delete the other at the end.
+    qubo_matrix = np.zeros((T_max, V + 1, 2, T_max, V + 1, 2), dtype=float)
     
-    # Path constraint
+    # Path constraint: for each t
+    # ( sum_{i,b} ( x_{t,i,b} ) - 1 ) ^2 
+    # = sum_{(i1,b1), (i2,b2)} ( x_{t,i1,b1} x_{t,i2,b2} ) + sum_{i,b} ( - 2 * x_{t,i,b} )  +  1        
+    T_qubo_matrix = lambda_t * (
+        np.ones((V+1, 2, V+1, 2), dtype=float) # + sum_{(i1,b1),(i2,b2)} x_{t,i1,b1} x_{t,i2,b2}
+        - 2 * np.array([1]+ ([0]*2*(V+1)+[1]) * (2*(V+1)-1), dtype=float).reshape((V+1,2,V+1,2)) # -2 * sum_{i,b} ( - x_{t,i,b} )
+    )
     for t in range(T_max):
-        for i, b in product(range(V), range(2)):
-            qubo_matrix[t, i, b, t, i, b] -= lambda_t
-            qubo_matrix[t, V, 0, t, i, b] += 2 * lambda_t
-                
-        qubo_matrix[t, V, 0, t, V, 0] -= lambda_t
-        
-        for i, j, bi, bj in product(range(V), range(V), range(2), range(2)):
-            if not (i == j and bi == bj):
-                qubo_matrix[t, i, bi, t, j, bj] += lambda_t
+        qubo_matrix[t, :, :, t, :, :] += T_qubo_matrix
     
-    # Set start/end nodes
-    start_nodes=set()
-    end_nodes= set()
-    for node, val in dict(graph.nodes.data('start')).items():
-        if val == 'start':
-            print(f'Found start node:{node}')
-            node_index_in_qubo = floor(nodes.index(node)/ 2)
-            start_nodes.add(node_index_in_qubo)
-        if val == 'end':
-            print(f'Found end node:{node}')
-            node_index_in_qubo = floor(nodes.index(node)/ 2)
-            end_nodes.add(node_index_in_qubo)
     
-    start_nodes = list(start_nodes)        
-    end_nodes = list(end_nodes)
-    print(f'Start nodes: {start_nodes}, End nodes: {end_nodes}')
-    exist_start_nodes = len(start_nodes) > 0
-    exist_end_nodes = len(end_nodes) > 0
+    # Graph step constraints: for each (t, t+1)
+    # 1 - sum_{i1, b1} ( x_{t,i1,b1} * ( sum_{i2,b2 neighbour i1, b1} x_{t+1,i2,b2} ) ) - 0.8 *  x_{t, end 0} * ( sum_{i, b} x_{t, i , b} )
+    # The middle term means the "1" is cancelled if x_{t,i1,b1} == 1 and x_{t, i2, b2} == 1 for a valid graph step 
+    # The last term allows new contigs to be started at low cost
+    G_qubo_matrix = np.zeros((V+1, 2, V+1, 2), dtype=float)
+    G_qubo_matrix[:, :, V, 0] = -1 * lambda_g # End node is always a neighbour
+    for u, v in graph.edges:
+        u_idx = nodes.index(u)
+        i, bi = u_idx // 2, u_idx % 2
+        v_idx = nodes.index(v)
+        j, bj = v_idx // 2, v_idx % 2
+        G_qubo_matrix[i, bi, j, bj] = -1 * lambda_g # - sum_{i1, b1} ( x_{t,i1,b1} * ( sum_{i2,b2 neighbour i1, b1} x_{t+1,i2,b2} ) )
+    for i, b in product(range(V), range(2)):
+        G_qubo_matrix[V, 0, i, b] = -0.8 * lambda_g # - 0.8 *  x_{t, end 0} * ( sum_{i, b} x_{t, i , b} )
     
-    # Graph step constraints
     for t in range(T_max - 1):
-        for i, j, bi, bj in product(range(V), range(V), range(2), range(2)):
-            if (nodes[2 * i + bi], nodes[2 * j + bj]) not in graph.edges:
-                qubo_matrix[t, i, bi, t+1, j, bj] += lambda_g
-        for i, bi in product(range(V), range(2)):
-            qubo_matrix[t, V, 0, t+1, i, bi] += 5 * lambda_g
-            if exist_end_nodes:
-                if i not in end_nodes:
-                    qubo_matrix[t, i, bi, t+1, V, 0] += lambda_g
-    if exist_start_nodes:
-        start_node = start_nodes[0]
-        for b in range(2):
-            qubo_matrix[0, start_node, b, 0, start_node, b] -= lambda_g
-            qubo_matrix[0, start_node, b, 0, start_nodes, 1 - b] += lambda_g
-        
-                
-    # Weights constraints
+        qubo_matrix[t, :, :, t+1, :, :] = G_qubo_matrix 
+    
+     
+    # Weight constraint
+    # for each i
+    # ( sum_{t,b} x_{t,i,b} - w(i) ) * ( sum_{t,b} x_{t,i,b} - w(i) + 0.5) 
+    # = w(i)(w(i) - 0.5) - (2w(i) - 0.5) * sum_{t,b} ( x_{t,i,b} ) + sum_{t1,b1,t2,b2} ( x_{t1,i,b1} x_{t2,i,b2})
+    def W_qubo_matrix(weight):
+        return lambda_w * (
+                np.ones((T_max, 2, T_max, 2), dtype=float) # sum_{t1,b1,t2,b2} ( x_{t1,i,b1} x_{t2,i,b2})
+                - (2 * weight - 0.5) * np.array([1]+ ([0]*2*(T_max)+[1]) * (2*(T_max)-1), dtype=float).reshape((T_max,2,T_max,2)) # - (2w(i) - 0.5) * sum_{t,b} ( x_{t,i,b} )
+            )
     for i in range(V):
-        for t in range(T_max):
-            for b in range(2):
-                qubo_matrix[t, i, b, t, i, b] -= (2 * graph.nodes[nodes[2 * i]]["weight"] - 1) * lambda_w
+        qubo_matrix[:, i, :, :, i, :] += W_qubo_matrix(graph.nodes[nodes[i]]["weight"])
         
-        for t1, t2 in product(range(T_max), range(T_max)):
-            for b1, b2 in product(range(2), range(2)):
-                if not (t1 == t2 and b1 == b2):
-                    qubo_matrix[t1, i, b1, t2, i, b2] += lambda_w
 
     qubo_matrix = qubo_matrix.reshape((T_max * (V+1) * 2), (T_max * (V+1) * 2))
     qubo_matrix = 0.5 * (qubo_matrix + qubo_matrix.T)
@@ -216,7 +177,11 @@ def qubo_matrix_from_graph(graph: nx.DiGraph, alpha: float | None=None, penaltie
     qubo_matrix = np.delete(qubo_matrix, [np.ravel_multi_index((t, V, 1), dims=(T_max, V+1, 2)) for t in range(T_max)], 0)
     qubo_matrix = np.delete(qubo_matrix, [np.ravel_multi_index((t, V, 1), dims=(T_max, V+1, 2)) for t in range(T_max)], 1)
     
-    offset = lambda_t * T_max  + lambda_w * int(sum(graph.nodes[nodes[2 * i]]["weight"] ** 2 for i in range(V))) + (1 if exist_start_nodes else 0)  * lambda_g
+    offset = (
+        lambda_t * T_max  
+        + lambda_g * (T_max - 1)
+        + lambda_w * sum([graph.nodes[nodes[2 * i]]["weight"] ** 2 - 0.5 * graph.nodes[nodes[2 * i]]["weight"] for i in range(V)])
+    )
     
     # normalisation = np.max(np.abs(qubo_matrix))
     # qubo_matrix = qubo_matrix / normalisation
