db5ac285c1d90d90f4b0466b6133033fe6fd1e54
James Bonfield <jkb@sanger.ac.uk>
2026-03-05 17:33:35 +0000

    Improvements to the copy number assignment.
    Note: only tested on minigraph GFAs and not the AlfaPang ones.
    
    The initial average depth assignment from which we adjace up or down
    by 30% was sometimes very far off when we have a predominance of
    low-depth (but non-zero) node noise.
    
    We now pick and average and gradually weed out low outliers to better
    tune the average, before phase 2 of depth-multiplier optimisation.
    
    For kmer2node + mqlib I found -f -d 4 --offset 0.5 worked well, but
    also -f -d 3 --offset 0.4 too is a good neighbour.  The defaults may
    need changing (and flipping so int/float is a CLI toggle), but it
    needs more exploration first.

--- tag_gfa_copy_numbers.pl
+++ tag_gfa_copy_numbers.pl
@@ -4,12 +4,19 @@
 # From here we turn it to a node coverage estimation. (Floating point)
 
 use strict;
+use Getopt::Long;
 
 my $verbose = 0;
-if ($ARGV[0] eq "-v") {
-    shift(@ARGV);
-    $verbose++;
-}
+my $offset = 0;
+my $float_copy = 0;
+my $min_depth = 0.1;
+my $depth_div = 4;
+
+GetOptions("v|verbose"    => \$verbose,
+	   "offset=f"     => \$offset,
+	   "f|float-copy" => \$float_copy,
+           "m|min-depth:f"=> \$min_depth,
+           "d|depth-div:f"=> \$depth_div);
 
 # Parse GFA
 my @node_order;
@@ -28,6 +35,17 @@ while (<>) {
         $node{$1} = $_;
         my @N = split("\t", $_);
         $seq{$N[1]} = $N[2];
+	my ($SC) = $node{$N[1]} =~ m/SC:f:(-?\d+(\.\d+)?)/;
+	if (!defined($SC)) {
+	    my ($KC) = $node{$N[1]} =~ m/KC:i:(\d+)/;
+	    if (!defined($KC)) {
+		print STDERR "No SC or KC present\n";
+		exit(1);
+	    }
+	    $SC = $KC / length($seq{$N[1]});
+	    $node{$N[1]} .= "\tSC:f:$SC";
+	}
+
     } elsif (/^L\s+(\S+)\s+(.)\s+(\S+)\s+(.)/) {
 	# ASSUMPTION: nodes before edges
 	if (!exists($node{$1}) || !exists($node{$3})) {
@@ -36,43 +54,63 @@ while (<>) {
 	}
         $edge[$edge_num] = $_;
 	$self_loop{$1} = 1 if $1 eq $3;
-#	if ($2 eq "+") {
-#	    push(@{$edge_out{$1}}, $edge_num);
-#	} else {
-#	    push(@{$edge_in{$1}}, $edge_num);
-#	}
-#	if ($4 eq "+") {
-#	    push(@{$edge_in{$3}},  $edge_num);
-#	} else {
-#	    push(@{$edge_out{$3}},  $edge_num);
-#	}
+	if ($2 eq "+") {
+	    push(@{$edge_out{$1}}, $edge_num);
+	} else {
+	    push(@{$edge_in{$1}}, $edge_num);
+	}
+	if ($4 eq "+") {
+	    push(@{$edge_in{$3}},  $edge_num);
+	} else {
+	    push(@{$edge_out{$3}},  $edge_num);
+	}
         $edge_num++;
     }
 }
 
 
-my $min_depth = 0.1;
-
-# An array of raw depths
+# An array of raw depths.  Filter very low figures (unused nodes), compute
+# average, and then cycle again with a revised estimate of what low-depth
+# means.
 my @depths = ();
 my $avg_depth = 0;
-my $total = 0;
-my $tlen = 0;
-foreach my $n (sort keys %node) {
-    my ($d) = $node{$n} =~ m/SC:f:(-?\d+(\.\d+)?)/;
-    if ($d > $min_depth) {
-	# For initial average, exclude self loops
-	next if exists($self_loop{$n});
+my $total;
+my $tlen;
 
-	push(@depths, $d);
-	$total += $d*(length($seq{$n}));
- 	$tlen += (length($seq{$n}));
+my $last_avg_depth;
+my $ncycles = 0;
+do {
+    @depths = ();
+    $last_avg_depth = $avg_depth;
+    $avg_depth = 0;
+    $total = 0;
+    $tlen = 0;
+
+    foreach my $n (sort keys %node) {
+	my ($d) = $node{$n} =~ m/SC:f:(-?\d+(\.\d+)?)/;
+	if ($d > $min_depth) {
+	    # For initial average, exclude self loops
+	    next if exists($self_loop{$n});
+
+	    # edge cases only
+	    next unless (exists($edge_in{$n})  && scalar(@{$edge_in{$n}})==0) ||  \
+		(exists($edge_out{$n}) && scalar(@{$edge_out{$n}})==0);
+
+	    push(@depths, $d);
+	    $total += $d*(length($seq{$n}));
+	    $tlen += (length($seq{$n}));
+	}
     }
-}
-$avg_depth = $total / $tlen;
-if ($verbose) {
-    print "Avg depth $avg_depth\n";
-}
+
+    $avg_depth = $total / $tlen;
+    if ($verbose) {
+	print "Avg depth $avg_depth\n";
+    }
+    $min_depth = $avg_depth/$depth_div;
+    $ncycles++;
+} while ($avg_depth != $last_avg_depth && $ncycles < 10);
+
+
 
 # When running on an trimmed sub-graph our stats can be skewed.
 # Rerun on the entire file to get a better estimate of the starting average
@@ -94,7 +132,7 @@ if ($verbose) {
 # Alternative; try fitting depth to D, 1*D, 2*D, etc.
 my $best_try=$avg_depth;
 my $best_delta=1e10;
-for (my $try=$avg_depth/1.3; $try<$avg_depth*1.3; $try+=0.5) {
+for (my $try=$avg_depth/1.3; $try<$avg_depth*1.5; $try+=0.1) {
     my $delta = 0;
 
     foreach my $n (sort keys %node) {
@@ -118,7 +156,7 @@ for (my $try=$avg_depth/1.3; $try<$avg_depth*1.3; $try+=0.5) {
 	$best_delta = $delta;
 	$best_try = $try;
     }
-    #print "try $try, delta=$delta\n";
+    print "try $try, delta=$delta\n" if $verbose;
 }
 
 if ($verbose) {
@@ -146,7 +184,11 @@ if ($verbose) {
     my @copy;
     foreach my $n (@node_order) {
 	my ($d) = $node{$n} =~ m/SC:f:(-?\d+(\.\d+)?)/;
-	push(@copy, int($d/$avg_depth+0.5));
+	if ($float_copy) {
+	    push(@copy, $d/$avg_depth+$offset);
+	} else {
+	    push(@copy, int($d/$avg_depth+0.5+$offset));
+	}
     }
     local $"=",";
     print "@copy\n";
